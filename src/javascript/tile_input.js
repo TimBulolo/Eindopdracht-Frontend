a39c5603882416dc09fcf96d05d03d37d3753fa3
TimBulolo <timufb@hotmail.com>
2022-10-06 22:16:10 +0200

    added waaning to tile script

--- src/javascript/tile_input.js
+++ src/javascript/tile_input.js
@@ -15,26 +15,26 @@ function randomTileInput() {
     });
     const randomTiles = document.getElementById('three-tiles');
     for (let i=0; i<3; i++) {
-        let randomTile = document.createElement('DIV');
+        let randomTile = document.createElement('img');
         randomTile.className = 'random-tile';
         randomTile.id = 'random-tile-' + i;
         randomTile.onclick = () => {
             sessionStorage.setItem('recipe', JSON.stringify(randomRecipes[i]));
             window.location.href = '../html/recipe.html'
         }
-        let tilePic = document.createElement('IMG');
+        let tilePic = document.createElement('img');
         tilePic.className = 'tile-pic';
         tilePic.src = randomPics[i];
-        let tileText = document.createElement('DIV');
+        let tileText = document.createElement('div');
         tileText.className = 'tile-text';
-        let recipeName = document.createElement('P');
+        let recipeName = document.createElement('p');
         recipeName.className = 'recipe-name';
         recipeName.textContent = randomTitles[i];
-        let tileInfo = document.createElement('DIV');
+        let tileInfo = document.createElement('div');
         tileInfo.className = 'tile-info';
-        let calIng = document.createElement('DIV');
+        let calIng = document.createElement('div');
         calIng.className = 'cal-ing';
-        let calAmount = document.createElement('P');
+        let calAmount = document.createElement('p');
         calAmount.textContent = randomCalories[i];
         const cal = document.createElement('p');
         cal.className = 'cal-ing-min';
@@ -74,6 +74,7 @@ function randomTileInput() {
 }
 
 import {searchedRecipes, searchedPics, searchedTitles, searchedCalories, searchedIngredientAmounts, searchedCookTimes} from "./search_request";
+import {searchTerm} from "./search_request";
 export {searchTileAppear, searchTileInput}
 
 function searchTileAppear() {
@@ -83,67 +84,89 @@ function searchTileAppear() {
 
 function searchTileInput() {
     const tiles = document.querySelectorAll('.recipe-tile')
-        tiles.forEach(tile => {
+    tiles.forEach(tile => {
         tile.remove();
     });
-    const searchedTiles = document.getElementById('searched-tiles');
-    for (let i=0; i<searchedRecipes.length; i++) {
-        let recipeTile = document.createElement('DIV');
-        recipeTile.className = 'recipe-tile';
-        recipeTile.id = 'searched-tile-' + i;
-        recipeTile.onclick = () => {
-            sessionStorage.setItem('recipe', JSON.stringify(searchedRecipes[i]));
-            window.location.href = '../html/recipe.html'
+    const retries = document.querySelectorAll('.retry');
+    retries.forEach(retry => {
+        retry.remove();
+    });
+    const bottomHalf = document.getElementById('bottom-half');
+    if (searchTerm.length < 1) {
+        const retry = document.createElement('div');
+        retry.className = 'retry'
+        const message = document.createElement('p');
+        message.textContent = 'Please enter a search query '
+        bottomHalf.appendChild(retry);
+        retry.appendChild(message);
+    } else {
+        if (searchedRecipes.length < 1) {
+            const retry = document.createElement('div');
+            retry.className = 'retry'
+            const message = document.createElement('p');
+            message.textContent = 'Your search didnt find any matches, please try something else '
+            bottomHalf.appendChild(retry);
+            retry.appendChild(message);
+        } else {
+            const searchedTiles = document.getElementById('searched-tiles');
+            for (let i = 0; i < searchedRecipes.length; i++) {
+                let recipeTile = document.createElement('div');
+                recipeTile.className = 'recipe-tile';
+                recipeTile.id = 'searched-tile-' + i;
+                recipeTile.onclick = () => {
+                    sessionStorage.setItem('recipe', JSON.stringify(searchedRecipes[i]));
+                    window.location.href = '../html/recipe.html'
+                }
+                let tilePic = document.createElement('img');
+                tilePic.className = 'tile-pic';
+                tilePic.src = searchedPics[i];
+                let tileText = document.createElement('div');
+                tileText.className = 'tile-text';
+                let recipeName = document.createElement('p');
+                recipeName.className = 'recipe-name';
+                recipeName.textContent = searchedTitles[i];
+                let tileInfo = document.createElement('div');
+                tileInfo.className = 'tile-info';
+                let calIng = document.createElement('div');
+                calIng.className = 'cal-ing';
+                let calAmount = document.createElement('p');
+                calAmount.textContent = searchedCalories[i];
+                const cal = document.createElement('p');
+                cal.className = 'cal-ing-min';
+                cal.textContent = '\u00A0Calories |\u00A0';
+                let ingAmount = document.createElement('p');
+                ingAmount.textContent = searchedIngredientAmounts[i];
+                const ing = document.createElement('p');
+                ing.className = 'cal-ing-min';
+                ing.textContent = '\u00A0Ingredients'
+                let time = document.createElement('div');
+                time.className = 'time';
+                const clockSymbol = document.createElement('p');
+                clockSymbol.className = 'cal-ing-min';
+                clockSymbol.textContent = '🕐\u00A0';
+                let timeAmount = document.createElement('p');
+                timeAmount.textContent = searchedCookTimes[i];
+                const min = document.createElement('p');
+                min.className = 'cal-ing-min';
+                min.textContent = '\u00A0min';
+
+                searchedTiles.appendChild(recipeTile);
+                recipeTile.appendChild(tilePic);
+                recipeTile.appendChild(tileText);
+                tileText.appendChild(recipeName);
+                tileText.appendChild(tileInfo);
+                tileInfo.appendChild(calIng);
+                calIng.appendChild(calAmount);
+                calIng.appendChild(cal);
+                calIng.appendChild(ingAmount);
+                calIng.appendChild(ing);
+                tileInfo.appendChild(time);
+                time.appendChild(clockSymbol);
+                time.appendChild(timeAmount);
+                time.appendChild(min);
+
+            }
+
         }
-        let tilePic = document.createElement('IMG');
-        tilePic.className = 'tile-pic';
-        tilePic.src = searchedPics[i];
-        let tileText = document.createElement('DIV');
-        tileText.className = 'tile-text';
-        let recipeName = document.createElement('P');
-        recipeName.className = 'recipe-name';
-        recipeName.textContent = searchedTitles[i];
-        let tileInfo = document.createElement('DIV');
-        tileInfo.className = 'tile-info';
-        let calIng = document.createElement('DIV');
-        calIng.className = 'cal-ing';
-        let calAmount = document.createElement('P');
-        calAmount.textContent = searchedCalories[i];
-        const cal = document.createElement('p');
-        cal.className = 'cal-ing-min';
-        cal.textContent = '\u00A0Calories |\u00A0';
-        let ingAmount = document.createElement('p');
-        ingAmount.textContent = searchedIngredientAmounts[i];
-        const ing = document.createElement('p');
-        ing.className = 'cal-ing-min';
-        ing.textContent = '\u00A0Ingredients'
-        let time = document.createElement('div');
-        time.className = 'time';
-        const clockSymbol = document.createElement('p');
-        clockSymbol.className = 'cal-ing-min';
-        clockSymbol.textContent = '🕐\u00A0';
-        let timeAmount =document.createElement('p');
-        timeAmount.textContent = searchedCookTimes[i];
-        const min = document.createElement('p');
-        min.className = 'cal-ing-min';
-        min.textContent = '\u00A0min';
-
-        searchedTiles.appendChild(recipeTile);
-        recipeTile.appendChild(tilePic);
-        recipeTile.appendChild(tileText);
-        tileText.appendChild(recipeName);
-        tileText.appendChild(tileInfo);
-        tileInfo.appendChild(calIng);
-        calIng.appendChild(calAmount);
-        calIng.appendChild(cal);
-        calIng.appendChild(ingAmount);
-        calIng.appendChild(ing);
-        tileInfo.appendChild(time);
-        time.appendChild(clockSymbol);
-        time.appendChild(timeAmount);
-        time.appendChild(min);
-
     }
-
 }
-
