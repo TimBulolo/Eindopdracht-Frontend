88df17d0a10d314fc0445a5bdc2b29e761a7671c
TimBulolo <timufb@hotmail.com>
2022-10-09 21:34:28 +0200

    calculator progress

--- src/javascript/calculator.js
+++ src/javascript/calculator.js
@@ -3,30 +3,60 @@ export let
     databaseKey = '',
     databaseId = 'edfb0da8';
 
-import {searchFood, searchNutrients} from "./database_request";
+import {searchFood, searchNutrients, product, weight, energy, fat, carbs,} from "./database_request";
 
-export let calSearchTermUrl = ''
-document.getElementById('calorie-search-bar').addEventListener("keyup", (e) => {
-    let calSearchTerm = e.target.value
-    if (isNaN(calSearchTerm)) {
-        calSearchTermUrl = `&ingr=${calSearchTerm}`
-    } else {
-        calSearchTermUrl = `&upc=${calSearchTerm}`
-    }
-});
+
 
 document.getElementById('calorie-search-form').addEventListener("submit", async (e) => {
     e.preventDefault();
     await searchFood();
+    fillTop();
 });
 
 
 
 
 document.getElementById('add-button').addEventListener("click", async () => {
-    await searchNutrients()
+    await searchNutrients();
+    fillBottom();
 })
 
+function fillTop() {
+    document.getElementById('product').textContent = product;
+    document.getElementById('quantity').textContent = weight;
+    document.getElementById('label').textContent = 'Gram';
+}
 
+function fillBottom() {
+    const productInput = document.getElementById('product-input');
+    const caloriesInput = document.getElementById('calories-input');
+    const fatInput = document.getElementById('fat-input');
+    const carbsInput = document.getElementById('carbs-input')
+    const productBox = document.createElement('div');
+    productBox.className = 'sum';
+    const productName = document.createElement('p');
+    productName.textContent = product
+    const calorieBox = document.createElement('div');
+    calorieBox.className = 'sum';
+    const energyKcal = document.createElement('p');
+    energyKcal.textContent = `${Math.ceil(energy[1])} ${energy[2]}`;
+    const fatBox = document.createElement('div');
+    fatBox.className = 'sum';
+    const fatGram = document.createElement('p');
+    fatGram.textContent = `${Math.ceil(fat[1])} ${fat[2]}`;
+    const carbsBox = document.createElement('div');
+    carbsBox.className = 'sum';
+    const carbsGram = document.createElement('p');
+    carbsGram.textContent = `${Math.ceil(carbs[1])} ${carbs[2]}`;
+
+    productInput.appendChild(productBox);
+    productBox.appendChild(productName);
+    caloriesInput.appendChild(calorieBox);
+    calorieBox.appendChild(energyKcal);
+    fatInput.appendChild(fatBox);
+    fatBox.appendChild(fatGram);
+    carbsInput.appendChild(carbsBox);
+    carbsBox.appendChild(carbsGram);
+}
 
 
--- src/javascript/database_request.js
+++ src/javascript/database_request.js
@@ -1,24 +1,80 @@
 import axios from "axios";
-import {databaseKey,databaseId, calSearchTermUrl, quantity} from "./calculator";
+import {databaseKey, databaseId} from "./calculator";
 
+let calSearchTermUrl = ''
+
+document.getElementById('calorie-search-bar').addEventListener("keyup", (e) => {
+    let calSearchTerm = e.target.value
+    if (isNaN(calSearchTerm)) {
+        calSearchTermUrl = `&ingr=${calSearchTerm}`
+    } else {
+        calSearchTermUrl = `&upc=${calSearchTerm}`
+    }
+});
+
+export let
+    product = '',
+    weight = '',
+    label = '';
 
 export async function searchFood() {
     try {
         const result = await axios.get(`https://api.edamam.com/api/food-database/v2/parser?app_id=${databaseId}&app_key=${databaseKey}${calSearchTermUrl}`);
         const results = result.data.hints;
-        const [searchedFood] =results;
-        foodId = searchedFood.food.foodId;
-        measureUri = searchedFood.measures[0].uri;
+        const [searchedFood] = results;
+        const {food} = searchedFood
+        foodId = food.foodId;
+        product = food.label;
+        let measurePick = searchedFood.measures;
+        let measureFind = await measurePick.find((serving) => {
+            return serving.label === 'Serving'
+        });
+        weight = measureFind.weight;
+        measureURI = measureFind.uri;
         console.log(searchedFood);
-        console.log(measureUri)
     } catch (err) {
         console.error(err)
     }
 }
 
+const amountInput = document.getElementById('amount-box');amountInput.addEventListener("input", (e) =>{
+    quantity = Number(e.target.value)
+});
+
+let foodId = '',
+    measureURI = '',
+    quantity = '';
 
 
+export async function searchNutrients() {
+    try {
+        const response = await axios({
+            method: 'post',
+            url: `https://api.edamam.com/api/food-database/v2/nutrients?app_id=${databaseId}&app_key=${databaseKey}`,
+            data: {
+                'ingredients': [
+                    {
+                        'quantity': quantity,
+                        'measureURI': measureURI,
+                        'foodId': foodId
+                    }
+                ]
+            }
+        });
+        const {ENERC_KCAL, FAT, CHOCDF} = response.data.totalNutrients;
+        const {label: eLabel, quantity: eQuantity, unit: eUnit} = ENERC_KCAL;
+        const {label: fLabel, quantity: fQuantity, unit: fUnit} = FAT;
+        const {label: cLabel, quantity: cQuantity, unit: cUnit} = CHOCDF;
+        energy = [eLabel, eQuantity, eUnit];
+        fat = [fLabel, fQuantity, fUnit];
+        carbs = [cLabel, cQuantity, cUnit];
+        console.log(cQuantity)
+    } catch (err) {
+        console.error(err)
+    }
+}
 
-// export async function searchNutrients() {
-//
-// }
+export let
+    energy = '',
+    fat = '',
+    carbs = '';
