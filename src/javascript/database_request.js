e357a7a8107134e116fd168683a4cddf23d04b42
TimBulolo <timufb@hotmail.com>
2022-10-11 20:57:09 +0200

    only styling and cleanup left

--- src/javascript/database_request.js
+++ src/javascript/database_request.js
@@ -1,52 +1,88 @@
 import axios from "axios";
 import {databaseKey, databaseId} from "./calculator";
+export {searchFood, searchNutrients};
 
-let calSearchTermUrl = ''
 
-document.getElementById('calorie-search-bar').addEventListener("keyup", (e) => {
-    let calSearchTerm = e.target.value
+const fillBar = document.getElementById('completed')
+const searchBar = document.getElementById('calorie-search-bar');
+searchBar.addEventListener("input", async(e) => {
+    calSearchTerm = e.target.value;
     if (isNaN(calSearchTerm)) {
-        calSearchTermUrl = `&ingr=${calSearchTerm}`
+        calSearchTermUrl = `&ingr=${calSearchTerm}`;
+        if (calSearchTerm.length > 1) {
+            await autoFill();
+            fillBar.value = completed;
+        }
     } else {
-        calSearchTermUrl = `&upc=${calSearchTerm}`
+        calSearchTermUrl = `&upc=${calSearchTerm}`;
+    }
+    if (calSearchTerm.length < 1) {
+        fillBar.value = ''
+    }
+    searchBar.onkeydown = (e) => {
+        if (e.key === 'ArrowRight') {
+            searchBar.value = fillBar.value;
+            calSearchTermUrl = `&ingr=${fillBar.value}`;
+        }
     }
 });
+
+let completed = '';
+
+async function autoFill() {
+    try {
+        const response = await axios.get(
+            `https://api.edamam.com/auto-complete?app_id=${databaseId}&app_key=${databaseKey}&q=${calSearchTerm}`);
+        completed = response.data[0];
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+let calSearchTerm = '',
+    calSearchTermUrl = ''
 
 export let
     product = '',
     weight = '',
     label = '';
 
-export async function searchFood() {
+async function searchFood() {
     try {
         const result = await axios.get(`https://api.edamam.com/api/food-database/v2/parser?app_id=${databaseId}&app_key=${databaseKey}${calSearchTermUrl}`);
         const results = result.data.hints;
         const [searchedFood] = results;
+        console.log(searchedFood);
         const {food} = searchedFood
         foodId = food.foodId;
         product = food.label;
         let measurePick = searchedFood.measures;
-        let measureFind = await measurePick.find((serving) => {
-            return serving.label === 'Serving'
-        });
+        let measureFind = await measurePick.find((serving) =>
+            serving.label === 'Serving' || 'Cup'
+        );
         weight = measureFind.weight;
         measureURI = measureFind.uri;
-        console.log(searchedFood);
+        console.log(result.config.url)
     } catch (err) {
         console.error(err)
     }
 }
 
-const amountInput = document.getElementById('amount-box');amountInput.addEventListener("input", (e) =>{
+const amountInput = document.getElementById('amount-box');
+amountInput.addEventListener("input", (e) => {
     quantity = Number(e.target.value)
 });
 
 let foodId = '',
     measureURI = '';
-export let quantity = '';
+export let
+    quantity = '',
+    energy = '',
+    fat = '',
+    carbs = '';
 
 
-export async function searchNutrients() {
+async function searchNutrients() {
     try {
         const response = await axios({
             method: 'post',
@@ -73,7 +109,8 @@ export async function searchNutrients() {
     }
 }
 
-export let
-    energy = '',
-    fat = '',
-    carbs = '';
+
+
+
+
+
